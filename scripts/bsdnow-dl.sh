234194c1c3365fec9f188f092e433421a35b64bb
Cj <cjmalone@mail.com>
2016-11-07 14:16:41 +0000

    Add a script to auto download bsdnow feeds

---
+++ scripts/bsdnow-dl.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+TMP=$(mktemp -d)
+
+curl "http://feeds.feedburner.com/BsdNowMp3" | xmllint --format - > "$TMP/MP3"
+curl "http://feeds.feedburner.com/BsdNowOgg" | xmllint --format - > "$TMP/OGG"
+curl "http://feeds.feedburner.com/BsdNowMobile" | xmllint --format - > "$TMP/MP4-low"
+curl "http://feeds.feedburner.com/BsdNowHd" | xmllint --format - > "$TMP/MP4-high"
+curl "http://bitlove.org/jupiterbroadcasting/bsdnowhd/feed" | xmllint --format - > "$TMP/MP4-bt"
+
+gedit "$TMP/MP3" "$TMP/OGG" "$TMP/MP4-low" "$TMP/MP4-high" "$TMP/MP4-bt" 2> /dev/null
+
+rm -rf "$TMP"
